645061618903de04132830ab600f79df6df53b26
William McKinnon <contact@willmckinnon.com>
2025-11-25 19:09:50 -0500

    added nix flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "rapidshell - an opinionated quickshell config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default-linux";
+  };
+
+  outputs = { self, nixpkgs, systems, ... }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
+
+    in {
+      # TODO: add homeManagerModule
+
+      packages = eachSystem (system: {
+        default = nixpkgs.legacyPackages.${system}.writeShellApplication {
+          name = "rapidshell";
+          text = "${nixpkgs.legacyPackages.${system}.quickshell}/bin/quickshell --path ${./src}";
+        };
+      });
+
+      devShells = eachSystem (system: {
+        default = nixpkgs.legacyPackages.${system}.mkShell {
+          name = "rapidshell dev shell";
+          packages = with nixpkgs.legacyPackages.${system}; [ quickshell ];
+        };
+      });
+  };
+}
+
